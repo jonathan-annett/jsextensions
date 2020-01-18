6cefd2304fd6b050627b14dd9e677841ca1f60cf
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-18 12:18:54 +1100

    remmoved polyfills.js (now in it's own repo jspolyfills)

--- src/compress.sh
+++ src/compress.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
-echo "compressing polyfills.js to polyfills.min.js"
-echo "/* jshint ignore:start */" > ./polyfills.min.js
-uglifyjs polyfills.js -c >> ./polyfills.min.js
-echo "/* jshint ignore:end */" >> ./polyfills.min.js
+echo "compressing extensions.js to extensions.min.js"
+echo "/* jshint ignore:start */" > ./extensions.min.js
+uglifyjs extensions.js -c >> ./extensions.min.js
+echo "/* jshint ignore:end */" >> ./extensions.min.js
 
-echo "compressing extensions.js to extensions.temp.js"
-echo "/* jshint ignore:start */" > ./extensions.temp.js
-uglifyjs extensions.js -c >> ./extensions.temp.js
-echo "/* jshint ignore:end */" >> ./extensions.temp.js
-
-echo "patching extensions.temp.js to extensions.min.js"
-node - << NODE
-var fs=require("fs");
-var src=fs.readFileSync("./extensions.temp.js","utf8");
-src=src.replace(/\.\/polyfills\.js/g,"./polyfills.min.js");
-fs.writeFileSync("./extensions.min.js",src);
-NODE
-
-node ./extensions.min.js --verbose
-uglifyjs ./extensions.min.js -b > ./extensions.min.fmt.js
 if [[ "$1" == "push" ]]; then
 cd ..
-git add src/extensions.js src/polyfills.js src/extensions.min.js src/polyfills.min.js  src/extensions.min.fmt.js
-git commit -m "updated polyfills.js and extensions.js"
+git add src/extensions.js src/extensions.min.js
+git commit -m "updated extensions.js"
 git push
 fi
