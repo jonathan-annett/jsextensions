64d7acd232d5691c16e05e1b5efbb867bfd48b53
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-18 15:36:02 +1100

    updated compress.sh to fetch jspolyfills from github

--- src/compress.sh
+++ src/compress.sh
@@ -3,10 +3,19 @@ echo "compressing extensions.js to extensions.min.js"
 echo "/* jshint ignore:start */" > ./extensions.min.js
 uglifyjs extensions.js -c >> ./extensions.min.js
 echo "/* jshint ignore:end */" >> ./extensions.min.js
-
-if [[ "$1" == "push" ]]; then
 cd ..
+NEW_POLY=0
+./get-latest-github.sh "https://github.com/jonathan-annett/jspolyfills.git" && NEW_POLY=1
+if [[ "${NEW_POLY}" == "1" ]]; then
+npm install
+fi
+if [[ "$1" == "push" ]]; then
 git add src/extensions.js src/extensions.min.js
+if [[ "${NEW_POLY}" == "1" ]]; then
+git add package.json
+fi
 git commit -m "updated extensions.js"
 git push
+echo -n current commit hash :
+git rev-parse HEAD
 fi
