807f6d228b8e38d99f9cb9849119ab56743161fa
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-28 20:11:46 +1100

    auto update

--- src/compress.sh
+++ src/compress.sh
@@ -5,6 +5,8 @@ node ..
 
 node - <<NODE
 
+var fs= require("fs");
+
 var js_zipWrap_js = fs.readFileSync("./js_zipWrap.js","utf8");
 js_zipWrap_js = makePackage("zipWrap",js_zipWrap_js);
 fs.writeFileSync("./js_zipWrap.pkg.js",js_zipWrap_js);
@@ -22,6 +24,22 @@ fs_jszip_browser_js     = makePackage("startFSJSZip",fs_jszip_browser_js);
 fs.writeFileSync("./fs_jszip-browser.pkg.js",fs_jszip_browser_js);
 fs_jszip_browser_js     = minifyJS(fs_jszip_browser_js);
 fs.writeFileSync("./fs_jszip-browser.min.js",fs_jszip_browser_js);
+
+require("..");
+
+function makePackage(name,pkg_fn){
+
+    var pkg_bare = pkg_fn.toString().trimEnd();
+
+    var template = packageTemplate.toString().trimEnd();
+    template = template.substring(template.indexOf('{')+1,template.length-1).trim().split('function acme_package(){}');
+    template.push(template.pop().split('${acme}').join(name));
+
+    return template.join(('function()'+pkg_bare.substring(pkg_bare.indexOf('{')).reindent(4)));
+}
+
+
+function packageTemplate(){(function(x){x[0][x[1]]=(function acme_package(){})();})(typeof process+typeof module+typeof require==='objectobjectfunction'?[module,"exports"]:[window,"${acme}"]);}
 
 
 NODE
