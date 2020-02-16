aa81e48b4d203e40934742670065f0368f786c1d
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-16 13:44:57 +1100

    auto update

--- src/jsextensions.js
+++ src/jsextensions.js
@@ -1,4 +1,4 @@
-/* non-minified concatenated source, built Sat Feb 15 10:42:29 AEDT 2020 from extensions.js */
+/* non-minified concatenated source, built Sun Feb 16 13:44:57 AEDT 2020 from extensions.js */
 /* js-sha1 */
 /*
  * [js-sha1]{@link https://github.com/emn178/js-sha1}
